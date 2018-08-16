dd77ffe0d38313241bd886bd2108612519fd7346
Stefaan Lippens <stefaan@netlog.com>
2018-08-16 10:57:44 +0200

    Finetune and harden python consumers

--- python/src/consume.py
+++ python/src/consume.py
@@ -1,27 +1,42 @@
-import sys
-import traceback
 import contextlib
+import logging
+import os
+
 from confluent_kafka import KafkaError
 from confluent_kafka.avro import AvroConsumer
-from confluent_kafka.avro.serializer import SerializerError
 
-c = AvroConsumer({
-    'bootstrap.servers': 'kafka:9092',
-    'group.id': 'SimpleConsumerExample',
-    'schema.registry.url': 'http://schemaregistry:8081',
-})
-topic = 'page_visits'
-c.subscribe([topic])
+log = logging.getLogger(os.path.basename(__file__))
 
-print("Starting to consume kafka topic {t!r}".format(t=topic))
 
-with contextlib.closing(c):
-    while True:
-        msg = c.poll(1)
-        if msg:
-            if not msg.error():
-                print(msg.value())
-            elif msg.error().code() != KafkaError._PARTITION_EOF:
-                print(msg.error())
+def main():
+    topic = 'page_visits'
+
+    c = AvroConsumer({
+        'bootstrap.servers': 'kafka:9092',
+        'group.id': 'SimpleConsumerExample',
+        'schema.registry.url': 'http://schemaregistry:8081',
+    })
+    c.subscribe([topic])
+
+    log.info("Starting to consume kafka topic {t!r}".format(t=topic))
+    with contextlib.closing(c):
+        while True:
+            try:
+                msg = c.poll(1)
+                if msg:
+                    if not msg.error():
+                        print(msg.value())
+                    elif msg.error().code() != KafkaError._PARTITION_EOF:
+                        log.error("Kafka error: %r", msg.error())
+                        continue
+            except KeyboardInterrupt:
+                log.info("kthxby!")
                 break
+            except Exception as e:
+                log.error("Consume failure: %r.", e, exc_info=True)
+                continue
 
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
--- python/src/consume_raw.py
+++ python/src/consume_raw.py
@@ -1,22 +1,40 @@
 import contextlib
+import logging
+import os
 
 from confluent_kafka import Consumer
 
-c = Consumer({
-    'bootstrap.servers': 'kafka:9092',
-    'group.id': 'groupraw',
-})
-topic = 'page_visits'
-c.subscribe([topic])
+log = logging.getLogger(os.path.basename(__file__))
 
-print("Starting to consume kafka topic {t!r}".format(t=topic))
 
-with contextlib.closing(c):
-    while True:
-        msg = c.poll(1)
-        if msg:
-            print((
-                repr(msg.key()),
-                repr(msg.value()),
-                msg.error()
-            ))
+def main():
+    topic = 'page_visits'
+
+    c = Consumer({
+        'bootstrap.servers': 'kafka:9092',
+        'group.id': 'raw.py',
+    })
+    c.subscribe([topic])
+
+    log.info("Starting to consume kafka topic {t!r}".format(t=topic))
+    with contextlib.closing(c):
+        while True:
+            try:
+                msg = c.poll(1)
+                if msg:
+                    print((
+                        msg.key(),
+                        msg.value(),
+                        msg.error()
+                    ))
+            except KeyboardInterrupt:
+                log.info("kthxby!")
+                break
+            except Exception as e:
+                log.error("Consume failure: %r.", e, exc_info=True)
+                continue
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
